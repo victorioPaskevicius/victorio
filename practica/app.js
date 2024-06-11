2ca1da881a6278ad5ff4f90bc375bc43ad96a549
victorio paskevicius <victorioxd7@gmail.com>
2024-06-11 17:21:28 -0300

    realizando commit para refrescar la organizacion de carpetas

---
+++ practica/app.js
@@ -0,0 +1,10 @@
+let expr = 'pera';
+
+switch (expr) {
+    case 'banana':
+        console.log('Es amarilla');
+        break;
+    case 'pera':
+        console.log('Es ovalada');
+        break;
+}
